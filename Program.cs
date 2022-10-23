5639044adeae1196d24681507baa129b3c9ba69f
Wellington <well_rafa@live.com>
2022-10-22 23:02:53 -0300

    Adiocionando o console.WriteLine

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ s.Ligar();
 s.Numero = "123456";
 s.ReceberLigacao();
 s.InstalarAplicativo("Doulingo");
+Console.WriteLine(s.Numero);
 
 Iphone i = new Iphone("Iphone", "12", "Iphone_1254_IP", 256);
 i.Numero = "654321";
